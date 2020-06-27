baadabc305afa8771907d07bd241d7c2cb0a4985
Ildar Akhmetgaleev <akhilman@gmail.com>
2020-06-28 00:46:01 +0700

    Move variables to template itself

--- generate.py
+++ generate.py
@@ -1,21 +1,10 @@
 from jinja2 import FileSystemLoader, Environment
 
 TEMPLATE_FILE = 'profile.pine'
-params = {
-    'n_rows': 20,
-    'n_bars': 25,
-    'avg_lenght': 20,
-    'row_multiplier': 0.6,
-    'block_colors': ['purple', 'green'],
-    'boundary_color': 'blue',
-    'recent_n_bars': 50,
-    'recent_block_color': 'gray'
-}
 
 template_loader = FileSystemLoader(searchpath='./')
 template_env = Environment(loader=template_loader)
 
 template = template_env.get_template(TEMPLATE_FILE)
-
-output = template.render(**params)
+output = template.render()
 print(output)
